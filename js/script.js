128d651c223d30ad7035ca8d6618b211b03fe66f
Isaac Sanchez <jelito@gmail.com>
2018-12-20 00:56:42 +0000

    🤦‍♂️ Update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
+// const jsonUrl = 'assets/pictures.json';
+// ('https://raw.githubusercontent.com/jelitter/CSWD-Assignment-2-Part-2/master/assets/pictures.json?token=AOi0_OdvPRL8aWAlAGyL7win6u26SPvyks5cJASTwA%3D%3D');
+const jsonUrl =
+  'https://raw.githubusercontent.com/jelitter/CSWD-Assignment-2-Part-2/master/assets/pictures.json?token=AOi0_OdvPRL8aWAlAGyL7win6u26SPvyks5cJASTwA%3D%3D';
 const memberWidth = 90;
-const step = 0.5;
+const step = 0.55;
 let caroussel, interval;
 let members, memberList, previousItem;
 let position = 0;
 
 const memberComponent = (name, picUrl) => `
-    <div class="member-picture" style="background-image: url('${picUrl}')">
-
-    </div>
+    <div class="member-picture" style="background-image: url('${picUrl}')"></div>
     <div class="member-name">${name}</div>
 `;
 
@@ -18,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   caroussel.children;
 
-  fetch('/assets/pictures.json')
+  fetch(jsonUrl)
     .then(res => res.json())
     .then(u => {
       members = u;
@@ -61,36 +63,41 @@ const animateCaroussel = () => {
     if (item != previousItem) {
       previousItem = item;
       const currentMember = memberList.children[item];
+      const picture = currentMember.children[0];
+      const name = currentMember.children[1];
 
       currentMember.style.setProperty('border', `solid 1px lightgray`);
       currentMember.style.setProperty('background-color', `white`);
       currentMember.style.setProperty('z-index', `10`);
       currentMember.style.setProperty('box-shadow', `1px 1px 1px rgba(0, 0, 0, 0.25)`);
       currentMember.style.setProperty('transform', `scale3d(1.1,1.1,1) rotate3d(0,0,1,${Math.sin(position) * 5}deg)`);
-      currentMember.children[0].style.setProperty('opacity', `1`);
-      currentMember.children[0].style.setProperty('border-bottom', 'solid 2px var(--main-color)');
-      currentMember.children[0].style.setProperty('background-color', `transparent`);
-      currentMember.children[0].style.setProperty('box-shadow', `none`);
-      currentMember.children[0].style.setProperty('border-radius', `0`);
-      currentMember.children[1].style.setProperty('opacity', `0.9`);
+      picture.style.setProperty('opacity', `1`);
+      picture.style.setProperty('border-bottom', 'solid 2px var(--main-color)');
+      picture.style.setProperty('background-color', `transparent`);
+      picture.style.setProperty('box-shadow', `none`);
+      picture.style.setProperty('border-radius', `0`);
+      name.style.setProperty('opacity', `0.9`);
 
       if (item > 0) {
         const previousMember = memberList.children[item - 1];
+        const picture = previousMember.children[0];
+        const name = previousMember.children[1];
+
         previousMember.style.setProperty('z-index', `1`);
         previousMember.style.setProperty('transform', `scale3d(0.975,0.975,1)`);
-        previousMember.style.setProperty('background-image', 'url("/assets/img/favicon.ico")');
+        previousMember.style.setProperty('background-image', 'url("./assets/img/favicon.ico")');
         previousMember.style.setProperty('background-repeat', 'no-repeat');
-        previousMember.children[0].style.setProperty('opacity', '0.5');
-        previousMember.children[0].style.setProperty('border-bottom', 'solid 2px gray');
-        previousMember.children[0].style.setProperty('transform', `scale3d(1,1,1)`);
-        previousMember.children[0].style.setProperty('background-color', `white`);
-        previousMember.children[1].style.setProperty('opacity', '0.25');
-        previousMember.children[1].style.setProperty('color', 'var(--main-color)');
-        previousMember.children[1].style.setProperty('transform', `scale3d(0.75,0.75,1)`);
+        picture.style.setProperty('opacity', '0.5');
+        picture.style.setProperty('border-bottom', 'solid 2px gray');
+        picture.style.setProperty('transform', `scale3d(1,1,1)`);
+        picture.style.setProperty('background-color', `white`);
+        name.style.setProperty('opacity', '0.25');
+        name.style.setProperty('color', 'var(--main-color)');
+        name.style.setProperty('transform', `scale3d(0.75,0.75,1)`);
       }
     }
 
-    // translate3d performs better then translateX as it uses the GPU
+    // translate3d performs better than translateX as it uses the GPU
     memberList.style.setProperty('transform', `translate3d(-${position}px,0,0)`);
   }, 16);
 };
